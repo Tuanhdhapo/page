cfeba2f4e367d082dc26d3dff5d31676794e9b67
Tuanhdhapo <tuan.hd@haposoft.com>
2021-10-28 17:24:08 +0700

    sort route

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,9 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('logout', [LogoutController::class, 'logout'])->name('logout');
 
 Route::resource('courses', CourseController::class)->only(['index','show']);
-
+Route::resource('courses.lessons', LessonController::class)->only(['show']);
 Route::middleware(['auth'])->group(function () {
     Route::get('courses/{course}/join', [CourseController::class, 'join'])->name('courses.join');
-    Route::resource('courses.lessons', LessonController::class)->only(['show']);
     Route::post('/course/review', [ReviewController::class, 'add'])->name('course.review');
     Route::get('/view/{file}', [DocumentController::class, 'show']);
     Route::resource('user', UserController::class)->only(['index', 'update']);
